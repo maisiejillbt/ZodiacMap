073fbb6c1399735a70982404a3b9a6f4aec17134
Maisie Bruno-Tyne <maisiejillb@gmail.com>
2021-10-12 09:12:02 -0700

    filtered json data and added all constellations

--- dist/assets/dataFilter.js
+++ dist/assets/dataFilter.js
@@ -1,23 +1,42 @@
+const fs = require('fs');
 const starData = require('./starData.json');
-const piscesHIP = [9487,113889,114971,3786,4906,5737,5743,7097,115830,116771,115738,116928,7007,7884,8833,8198,7535,6706,4889,5586,6193,5742,5571,5131,5132,5310,5454,118268]
-const pisces = []
-const geminiHIP = [37826,36850,31681,30343,32246,29655,32362,35550,37740,35350,33018,36046,34088,36962,30883,28734,36366,37629,34693,32249,33202,37265,37908,39424,38538]
-const gemini = []
+const constellations = require('./constellations.json');
+const HIP = []; 
+const HD = []; 
+const filteredData = []; 
 
-for(let i = 0; i < 119618; i++){
-  if(piscesHIP.includes(parseInt(starData[i]["HIP"]))) {
-    const x = starData[i]["X"];
-    const y = starData[i]["Y"];
-    const z = starData[i]["Z"];
-    pisces.push([x,y,z]);
-  }else if(geminiHIP.includes(parseInt(starData[i]["HIP"]))) {
-    const x = starData[i]["X"];
-    const y = starData[i]["Y"];
-    const z = starData[i]["Z"];
-    gemini.push([x,y,z]);
+for(let i = 0; i < constellations.length; i++) {
+  if(constellations[i]["HIP"]){
+    HIP.push(constellations[i]["HIP"]);
   }
 }
 
-console.log(pisces);
-console.log(gemini);
-// console.log(pisces.length)
+for(let i = 0; i < constellations.length; i++) {
+  if(constellations[i]["HD"]){
+    HD.push(constellations[i]["HD"]);
+  }
+}
+
+// 119618
+
+for(let i=0; i<119618;i++){
+  let starHIP = parseInt(starData[i]["HIP"]);
+  let starHD = parseInt(starData[i]["HD"]);
+  let star = starData[i];
+
+  if(starHIP && HIP.includes(starHIP)) {
+    filteredData.push(star);
+  }else if(starHD && HD.includes(starHD)){
+    filteredData.push(star);
+  }
+}
+
+const data = JSON.stringify(filteredData);
+
+// write JSON string to a file
+fs.writeFile('filteredStars.json', data, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("JSON data is saved.");
+});
--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,7 +1,13 @@
-const starData = require('../../dist/assets/starData2.json');
+const starData = require('../../dist/assets/filteredStars.json');
 
-function test() { 
-  console.log(starData[0])
+function getXYZ() { 
+  const xyz = []; 
+  for(let i=0;i<starData.length;i++){
+    xyz.push(starData[i]["X"]);
+    xyz.push(starData[i]["Y"]);
+    xyz.push(starData[i]["Z"]);
+  }
+  return xyz; 
 }
 
-export {starData}
+export {starData, getXYZ}
